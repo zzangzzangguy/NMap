9690c0ab1e48580211a25d13ab97ef7957004c8b
김기현 <creeps1104@gmail.com>
2025-03-10 18:44:36 +0900

    Initial commit: SPM wrapper for Naver Maps SDK

--- Sources/NMap/NMap.swift
+++ Sources/NMap/NMap.swift
@@ -8,7 +8,7 @@ import NMapsGeometry
 public struct NMap {
     /// 네이버 맵 SDK 초기화를 위한 메소드
     public static func initialize(clientId: String) {
-        NMapsView.setClientId(clientId)
+        NMFAuthManager.shared().clientId = clientId
     }
 
     /// 새로운 지도 뷰 인스턴스 생성
@@ -16,10 +16,10 @@ public struct NMap {
         return NMFMapView()
     }
 
-    /// 현재 SDK 버전 반환
-    public static var sdkVersion: String {
-        return NMFAuthManager.shared().sdkVersion
-    }
+    // 현재 SDK 버전 반환 - 실제로 이 메서드가 있는지 확인 필요
+    // public static var sdkVersion: String {
+    //     return NMFAuthManager.shared().sdkVersion
+    // }
 
     // 초기화 방지
     private init() {}
